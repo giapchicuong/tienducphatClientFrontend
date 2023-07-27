66d49107b24cbfb7bec851d33f21efe06806b4d6
Giap Chi Cuong <giapchicuong@gmail.com>
2023-07-27 15:51:31 +0700

    update

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -9,9 +9,18 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
+      const productToAdd = action.payload;
+      const existingProduct = state.products.find((item) => item._id === productToAdd._id);
+
+      if (existingProduct) {
+        // If the product already exists in the cart, just increment its quantity.
+        existingProduct.quantity += 1;
+      } else {
+        // If it's a new product, add it to the cart with quantity 1.
+        state.products.push({ ...productToAdd, quantity: 1 });
+      }
+
       state.quantity += 1;
-      state.products.push(action.payload);
-      // state.total = state.products.reduce((a,i) => a + i.quantity * i.price,0).toFixed(0);
     },
     removeProduct: (state,action) => {
       state.products.splice(
